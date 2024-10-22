1bfa0b585a1f435857af20739fb34e34e6071a38
Angel <151583275+Anng3l@users.noreply.github.com>
2024-10-22 07:57:22 -0500

    Aumento 2

--- moduloDos.js
+++ moduloDos.js
@@ -2,3 +2,7 @@ import x, { nombre, rangos } from './modulosUno.js';
 
 console.log("Mejor champ top: ", nombre); 
 x(nombre);
+
+console.log("\n####################################################################\n")
+let rank = "bronce"
+rangos(rank);
